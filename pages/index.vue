53ab9105cdd69a4749a53d9cd081df41979e6f52
MoeRayo <moronfolu.motunrayo1@gmail.com>
2022-03-22 20:46:25 +0100

    feat: header creation

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    
+    <nav class="pa3 pa4-ns">
+      <a class="link dim black b f1-ns f3 tc db mb3 mb4-ns">Barcode Image Generator</a>
+    </nav>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+};
 </script>
+
+ 
